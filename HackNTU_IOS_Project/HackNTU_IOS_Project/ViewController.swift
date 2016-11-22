05245c863d2f0004fc8b4b09e763a98562c1098c
何柏融 <peter810725@hotmail.com.tw>
2016-11-23 00:28:13 +0800

    Create first single view empty project, first commit.

---
+++ HackNTU_IOS_Project/HackNTU_IOS_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackNTU_IOS_Project
+//
+//  Created by 何柏融 on 2016/11/23.
+//  Copyright © 2016年 Peter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
