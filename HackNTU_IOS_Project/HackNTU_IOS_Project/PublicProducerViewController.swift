4bcc2dd7495e1a5ead5aa0a1d05c8e86f7aa69db
Allen <bboypupu@gmail.com>
2016-12-21 17:44:22 +0800

    Tracking distance in PublicProducerView

--- HackNTU_IOS_Project/HackNTU_IOS_Project/PublicProducerViewController.swift
+++ HackNTU_IOS_Project/HackNTU_IOS_Project/PublicProducerViewController.swift
@@ -18,7 +18,9 @@ class PublicProducerViewController: UIViewController {
     @IBOutlet weak var durationLabel: UILabel!
     
     var userLocation:[CLLocation] = []
+    var tracedDistance:[CLLocationDistance] = []
     let manager = CLLocationManager()
+    var cumulativeDistance: Float = 0.0
     
     func checkAuthorityOfMap() {
         let status = CLLocationManager.authorizationStatus()
@@ -30,13 +32,14 @@ class PublicProducerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Ready for Tracking")
-        manager.requestWhenInUseAuthorization()
+//        manager.requestWhenInUseAuthorization()
+        manager.requestAlwaysAuthorization()
         trackerMapView.userTrackingMode = .follow
         manager.desiredAccuracy = kCLLocationAccuracyBest
 //        manager.distanceFilter = 10
         manager.delegate = self
         trackerMapView.delegate = self
-        
+        distanceLabel.text = "0.0"
     }
 
     override func didReceiveMemoryWarning() {
@@ -76,15 +79,28 @@ extension PublicProducerViewController: CLLocationManagerDelegate {
             let area = [userLocation[startIndex].coordinate, userLocation[endIndex].coordinate]
             let polyLine = MKPolyline(coordinates: area, count: area.count)
             trackerMapView.add(polyLine)
+            tracedDistance.append(userLocation[startIndex].distance(from: userLocation[endIndex]))
         }
     }
 }
 
 extension PublicProducerViewController: MKMapViewDelegate {
-    func updateMapView(_ mapView: MKMapView, renderFor overlay: MKOverlay) -> MKOverlayRenderer {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let polylineRenderer = MKPolylineRenderer(overlay: overlay)
         polylineRenderer.lineWidth = 5
-        polylineRenderer.strokeColor = .red
+        let nowSpeed = userLocation.last!.speed
+        if tracedDistance.last != nil {
+            cumulativeDistance += Float(tracedDistance.last!)
+        }
+        
+        if nowSpeed < 20 {
+            polylineRenderer.strokeColor = .green
+        }
+        else {
+            polylineRenderer.strokeColor = .red
+        }
+        
+        distanceLabel.text = String(cumulativeDistance)
         
         return polylineRenderer
     }
